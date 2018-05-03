25c26b9f128fa3d4d48c58110583cee23384d4ee
Sanjeev Kumar <sanjeevkumar761@gmail.com>
2018-05-03 16:52:22 +0200

    updated port to 80

--- index.js
+++ index.js
@@ -129,7 +129,7 @@ function coreFn2(auth){
 			}
 		  });		
 	});
-	app.listen(3000, () => console.log('Example app listening on port 3000!'))	
+	app.listen(80, () => console.log('Example app listening on port 3000!'))	
 }
 
 /**
