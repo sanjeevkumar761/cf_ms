fb0afe0a2a000baec1ea49675df03503a73d7cba
S0019146386 <sanjeev_kumar@rcomext.com>
2018-05-08 09:09:44 +0000

    Added CORS support

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ const TOKEN_PATH = 'credentials.json';
 
 var express = require('express')
 var app = express()
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 //Transform the content of cash flow data rows and columns here
 function transformData(rows){
