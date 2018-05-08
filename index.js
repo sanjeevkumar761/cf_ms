0d534d06923fb694d0c96b54d158fdbe6137ab8c
S0019146386 <sanjeev_kumar@rcomext.com>
2018-05-08 10:38:40 +0000

    added

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const TOKEN_PATH = 'credentials.json';
 
 var express = require('express')
 var app = express()
+app.use(express.static('public'))
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
