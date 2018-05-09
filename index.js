4171ba7cd767168c4477e53b2f7e4025c905aabb
Rolf Bühlmann <rolf_buehlmann@swissre.com>
2018-05-09 09:33:52 +0000

    My 2st Change

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ function coreFn2(auth){
 		  sheets.spreadsheets.values.get({
 			//spreadsheetId: '1rWK0TueCetHp7SSUVj1y8Y4TdCv0RIHPog7BBxOrqGM',
 			spreadsheetId: '1rWK0TueCetHp7SSUVj1y8Y4TdCv0RIHPog7BBxOrqGM',
-			range: 'Sheet1!A:AQ',
+			range: 'RolfSheet!A:D',
 		  }, (err, {data}) => {
 			if (err) return console.log('The API returned an error: ' + err);
 			const rows = data.values;
@@ -134,7 +134,7 @@ function coreFn2(auth){
 	app.get('/upsert', function (req, res) {
 		sheets.spreadsheets.values.append({
 			spreadsheetId: '1FuLa7ZP_5e1gQoP9rqlMDf33_SiZLmAKiBwJUyjP630',
-			range: 'Sheet1!A2:E',
+			range: 'RolfSheetOne!A2:E',
 		  valueInputOption: 'RAW',
 		  insertDataOption: 'INSERT_ROWS',
 		  resource: {
@@ -170,7 +170,7 @@ function coreFn2(auth){
 				//Write now
 				sheets.spreadsheets.values.append({
 					spreadsheetId: '1FuLa7ZP_5e1gQoP9rqlMDf33_SiZLmAKiBwJUyjP630',
-					range: 'Sheet1!A2:E',
+					range: 'RolfSheetOne!A2:E',
 				  valueInputOption: 'RAW',
 				  insertDataOption: 'INSERT_ROWS',
 				  resource: {
